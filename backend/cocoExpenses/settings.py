fbe4de9d3121b018027461402767f1886fbd35dc
Cosimo Russo <cosimo.russo@fiscozen.it>
2025-09-27 12:04:13 +0200

    Load configurations from env

--- backend/cocoExpenses/settings.py
+++ backend/cocoExpenses/settings.py
@@ -20,14 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bqlw*p9x+wlszh5i08!$f(c-*#&^^n)r$9l!310400m7$6f_1*"
+ENV: Literal["local", "production"] = os.getenv("ENV")
+if ENV not in ["local", "production"]:
+    raise ValueError("ENV must be either local or production")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ENV == "local"
 
 ALLOWED_HOSTS = ['backend', 'localhost']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://localhost', 'http://backend']
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
 
 
 # Application definition
