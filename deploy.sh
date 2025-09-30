fcebdee42fa3f1b38bf5dea09793a18f60ac214a
Cosimo Russo <cosimo.russo@fiscozen.it>
2025-09-30 23:33:29 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 docker compose -f docker-compose-prod.yml down &&
 git pull && 
 bash build_frontend.sh && 
-bash restart_prod.sh
+docker compose -f docker-compose-prod.yml build &&
+docker compose -f docker-compose-prod.yml up -d
