830e2af1a8aefb6726805326240a8faafd7b1f61
Cosimo Russo <cosimo.russo123@gmail.com>
2025-11-06 20:41:55 -0300

    Alter lenght of currency display name

--- backend/expenses/models/currency.py
+++ backend/expenses/models/currency.py
@@ -3,7 +3,7 @@ from django.db import models
 class Currency(models.Model):
     code = models.CharField(max_length=10)
     symbol = models.CharField(max_length=10)
-    display_name = models.CharField(max_length=10)
+    display_name = models.CharField(max_length=255)
 
     def __str__(self):
         return f"{self.display_name} ({self.code})"
