a2ebfe5467bce277f103906ee67b3c767e6e662c
Cosimo Russo <cosimo.russo123@gmail.com>
2025-11-13 18:34:47 -0300

    Implement model DollarExchangeRate

--- backend/expenses/models/__init__.py
+++ backend/expenses/models/__init__.py
@@ -4,5 +4,6 @@ from .expense_category import ExpenseCategory
 from .trip import Trip
 from .currency import Currency
 from .user_settings import UserSettings
+from .dollar_exchange_rate import DollarExchangeRate
 
-__all__ = ["User", "Expense", "ExpenseCategory", "Trip", "Currency", "UserSettings"]
+__all__ = ["User", "Expense", "ExpenseCategory", "Trip", "Currency", "UserSettings", "DollarExchangeRate"]
