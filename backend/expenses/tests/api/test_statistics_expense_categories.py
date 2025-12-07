6171ced1e14937a691e0d5c6e0fb42505ff95836
Antolina <51455932+AntolinaGonzalez@users.noreply.github.com>
2025-12-07 11:58:42 -0300

    Fix tests with error + new test for amortization (#10)
    * tests
    
    * test amortization

--- backend/expenses/tests/api/test_statistics_expense_categories.py
+++ backend/expenses/tests/api/test_statistics_expense_categories.py
@@ -26,8 +26,7 @@ class StatisticsExpenseCategoriesTestCase(ApiTestCase):
             ExpenseFactory(  # 25 euro a day for 4 days
                 user=cls.user,
                 expense_date=date_utils.today(),
-                forecast_amount=100,
-                actual_amount=100,
+                amount=100,
                 category=cls.category_1,
                 amortization_start_date=date_utils.today(),
                 amortization_end_date=date_utils.today() + timedelta(days=3),
@@ -35,8 +34,7 @@ class StatisticsExpenseCategoriesTestCase(ApiTestCase):
             ExpenseFactory(  # 20 euro a day for 5 days, forecast only
                 user=cls.user,
                 expense_date=None,
-                forecast_amount=100,
-                actual_amount=None,
+                amount=100,
                 category=cls.category_1,
                 amortization_start_date=date_utils.today(),
                 amortization_end_date=date_utils.today() + timedelta(days=4),
@@ -47,8 +45,7 @@ class StatisticsExpenseCategoriesTestCase(ApiTestCase):
             ExpenseFactory(  # 10 euro a day for 10 days
                 user=cls.user,
                 expense_date=date_utils.today(),
-                forecast_amount=100,
-                actual_amount=100,
+                amount=100,
                 category=cls.category_2,
                 amortization_start_date=date_utils.today() - timedelta(days=5),
                 amortization_end_date=date_utils.today() + timedelta(days=4),
@@ -56,8 +53,7 @@ class StatisticsExpenseCategoriesTestCase(ApiTestCase):
             ExpenseFactory(  # 10 euro a day for 1 day, old one
                 user=cls.user,
                 expense_date=date_utils.today() + dt.timedelta(days=10),
-                forecast_amount=10,
-                actual_amount=10,
+                amount=10,
                 category=cls.category_2,
                 amortization_start_date=date_utils.today() - timedelta(days=10),
                 amortization_end_date=date_utils.today() - timedelta(days=10),
@@ -84,9 +80,67 @@ class StatisticsExpenseCategoriesTestCase(ApiTestCase):
         self.assertEqual(len(response.data), 2)
         # Category 1
         self.assertEqual(response.data[0]["category"]["code"], self.category_1.code)
-        self.assertEqual(response.data[0]["forecast_amount"], "90.00")
-        self.assertEqual(response.data[0]["actual_amount"], "50.00")
+        self.assertEqual(response.data[0]["amount"], "90.00")
         # Category 2
         self.assertEqual(response.data[1]["category"]["code"], self.category_2.code)
-        self.assertEqual(response.data[1]["forecast_amount"], "20.00")
-        self.assertEqual(response.data[1]["actual_amount"], "20.00")
+        self.assertEqual(response.data[1]["amount"], "20.00")
+
+
+class StatisticsExpensesAmortizationTestCase(ApiTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+        cls.non_expense = CategoryFactory(
+            user=cls.user, for_expense=False, code="income", name="salary"
+        )
+        cls.rent = CategoryFactory(
+            user=cls.user, for_expense=True, code="rent", name="house"
+        )
+
+        cls.non_expense = [
+            ExpenseFactory( 
+                user=cls.user,
+                expense_date=date_utils.today(),
+                amount=100,
+                category=cls.non_expense,
+                amortization_start_date=date_utils.today(),
+                amortization_end_date=date_utils.today() + timedelta(days=30),
+                is_expense=False,
+            ),
+        ]
+
+        cls.rent = [
+            ExpenseFactory(  
+                user=cls.user,
+                expense_date=date_utils.today(),
+                amount=30,
+                category=cls.rent,
+                amortization_start_date=date_utils.today(),
+                amortization_end_date=date_utils.today() + timedelta(days=30),
+            ),
+        ]
+
+    def url(self, start_date: dt.date, end_date: dt.date):
+        return (
+            f"{reverse('expenses:statistics-amortization-timeline')}"
+            f"?start_date={start_date.isoformat()}&end_date={end_date.isoformat()}"
+        )
+
+    def setUp(self):
+        self.login(email=self.user.email)
+
+    def test_statistics_expense_categories(self):
+        """
+        Test the statistics expense amortization endpoint.
+        """
+
+        today = date_utils.today()
+        end_month = date_utils.today() + timedelta(days=30)
+        response = self.client.get(self.url(start_date=today, end_date=end_month))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(response.data[0]["date"], today.isoformat())
+        self.assertEqual(response.data[0]["expense_amount"], "0.97")
+        self.assertEqual(response.data[0]["non_expense_amount"], "3.23")
+        self.assertEqual(response.data[0]["difference"], "2.26")
+
