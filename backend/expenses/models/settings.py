60ef65de1d040e913ab9d1491d2c44d50cdb8b6a
Cosimo Russo <cosimo.russo123@gmail.com>
2026-01-06 19:10:13 +0000

    Make settings a singleton

--- backend/expenses/models/settings.py
+++ backend/expenses/models/settings.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class Settings(models.Model):
+    singleton = models.BooleanField(default=True, unique=True, editable=False)
     max_categories = models.PositiveIntegerField(null=True, blank=True)
     max_trips = models.PositiveIntegerField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.singleton = True
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Settings (max_categories={self.max_categories}, max_trips={self.max_trips})"
