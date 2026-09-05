90748df5f66b999b456bc4c96eb6d9ad5d87c4aa
Cosimo Russo <cosimo.russo123@gmail.com>
2026-09-05 15:30:30 +0000

    Add migrate step to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ docker compose -f docker-compose-prod.yml down &&
 git pull && 
 bash build_frontend.sh && 
 docker compose -f docker-compose-prod.yml build &&
-docker compose -f docker-compose-prod.yml up -d
+docker compose -f docker-compose-prod.yml up -d &&
+docker compose -f docker-compose-prod.yml exec backend python manage.py migrate
